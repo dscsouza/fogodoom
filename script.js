2167582cda9cd109b652e240c78428376f27f79f
dscsouza <dscsouza@gmail.com>
2022-06-09 01:50:23 -0400

    faltaLogicaFogoSubindo

---
+++ script.js
@@ -0,0 +1,109 @@
+let intensidade = [];
+let intensidadeInicial = 35;
+let canvas = [];
+let x = 10;
+let y = 15;
+let htmlTable = "";
+let development = false;
+let estilo = ""
+const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
+
+
+
+function start(){
+    // varreArray()
+    dadosIniciais()
+    desenhaTabela()
+    console.log(fireColorsPalette)
+
+    setInterval(() => {
+        varreArray();
+
+        desenhaTabela()
+        
+    }, 1000);
+
+}
+
+start();
+
+
+function desenhaTabela(){
+    lengthCanvas = x * y;
+    tabela = document.querySelector("#canvas");
+    num = 0;
+    intensidadeInicial -=  (y-1);
+
+    for (i = 0; i < y; i++) {
+        htmlTable += `<tr>`
+        num = i * x;
+        for (j = 0; j < x; j++) {
+            //se estiver em ambiente de desenvolvimento
+            //ele popula o array com números na intensidade
+            if (development){
+                intensidade[num + j] = intensidadeInicial;
+            }
+            
+
+            estilo = (development)?`color:${renderizaIntensidade(num + j)}`:`background-color:${renderizaIntensidade(num + j)}`;
+            htmlTable += (development)?`  <td style="${estilo}">
+                                <span class="index">${num + j}</span>
+                                <span class="intense">${intensidade[num + j]}</span>
+                            </td>`: `<td style="${estilo}"></td>`;
+            
+        }
+        htmlTable += `</tr>`
+        if (development){
+            intensidadeInicial++; 
+        }
+        
+    }
+
+    
+
+    tabela.innerHTML = htmlTable;
+
+}
+
+function renderizaIntensidade(pos){
+  
+  return `rgb(${fireColorsPalette[intensidade[pos]].r}, ${fireColorsPalette[intensidade[pos]].g}, ${fireColorsPalette[intensidade[pos]].b})`
+
+}
+
+function varreArray(){
+
+    for (i = y-2; i != 0; i--) {
+        num = i * x;
+        for (j = x; j != 0; j--) {
+            if (intensidade[num+j+x] !=0){
+                payload = Math.floor(Math.random() * intensidade[num+j+x]);
+                intensidade[num + j] = intensidade[num+j+x] - payload;
+            }
+        }
+    }
+    //continuar daqui, falta implementar a lógica para varrer array
+
+    // for (i = (x*y-1); i = 0; i--) {
+    //     payload = Math.floor(Math.random() * 35)                      
+    //     intensidade[num + j] = intensidadeInicial - payload;       
+    // }
+
+
+}
+
+
+function dadosIniciais(){
+    
+    
+    for (let i = 0; i < x * y; i++){
+        if ((i < x*y) & i >= (x*y-x)){
+            intensidade[i]=35;
+        }else{
+            intensidade[i]=0;
+        }
+    }
+    console.log(intensidade);
+}
+
+
